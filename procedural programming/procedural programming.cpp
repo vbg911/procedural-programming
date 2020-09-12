a9a79986f5c3aaced252d01ee5257bb4b90aef63
vbg911 <vbg911@gmail.com>
2020-09-12 20:42:30 +0300

    updated LampWithCurtain()

--- procedural programming/procedural programming.cpp
+++ procedural programming/procedural programming.cpp
@@ -176,14 +176,58 @@ void anotherEquation() {
 
 // 5 задача
 void LampWithCurtain() {
-	bool isDay, isCurtainOpen, isLampOn;
+	string isDay, isCurtainOpen, isLampOn;
+	bool day, curtain, lamp;
 	cout << "На улице день? Шторы отктрыты? Лампа включена?\n";
 	cout << "Введите true/false 3 раза через пробел.\n";
 	cin >> isDay >> isCurtainOpen >> isLampOn;
-	cout << "На улице день? " << boolalpha<< isDay <<"\n"<< "Шторы открыты? " << boolalpha << isCurtainOpen <<"\n"<< "Лампа включена? " << boolalpha << isLampOn;
-}
+	if (isDay == "true") {
+		day = true;
+	}else if (isDay == "false") {
+		day = false;
+	}
+	else
+	{
+		cout << "Время суток не распознано. По умолчарию будет использовано значение 'false'.\n";
+		day = false;
+	}
 
+	if (isCurtainOpen == "true") {
+		curtain = true;
+	}
+	else if (isCurtainOpen == "false") {
+		curtain = false;
+	}
+	else
+	{
+		cout << "Состояние штор не распознано. По умолчарию будет использовано значение 'false'.\n";
+		curtain = false;
+	}
 
+	if (isLampOn == "true") {
+		lamp = true;
+	}
+	else if (isLampOn == "false") {
+		lamp = false;
+	}
+	else
+	{
+		cout << "Состояние лампы не распознано. По умолчарию будет использовано значение 'false'.\n";
+		lamp = false;
+	}
+
+	cout << "На улице день? " << boolalpha<< day <<"\n"<< "Шторы открыты? " << boolalpha << curtain <<"\n"<< "Лампа включена? " << boolalpha << lamp<<endl;
+	if (day && curtain) {
+		cout << "В команате светло!" << endl;
+	} else if (lamp){
+		cout << "В команате светло!" << endl;
+	} else {
+		cout << "В команате темно!" << endl;
+	}
+		
+	}
+
+ 
 
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
