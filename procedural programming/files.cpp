1e479ea423be537c5c7229baf5a8733d6332a2cf
vbg <vbg911@gmail.com>
2020-10-26 00:44:46 +0300

    some changes

--- procedural programming/files.cpp
+++ procedural programming/files.cpp
@@ -12,18 +12,31 @@ string from_dec_farc(double decfrac, int newsys);
 
 void processing_file() {
 	system("cls");
-	int nums;
-	string decnum;
+	int nums, numbersystem;
+	string decnum ,readnum;
 	cout << "Создать файл из N вещественных чисел в десятичной системе счисления.\nВ другой файл записать эти числа, переведенные из исходного файла в любую систему счисления по желанию пользователя в пределах от 2 - ой до 9 - ой.";
-	cout << "\nВведите количество чисел.";
+	cout << "\nВведите количество чисел :";
 	cin>> nums;
+	cout << "\nВведите конечную систему счисления :";
+	cin >> numbersystem;
 	ofstream fout("decnums.txt");
 	for (int i=0; i<nums;i++) {
-		cout << "\nВведите число №" << i+1 << endl;
+		cout << "\nВведите число с точкой №" << i+1 << endl;
 		cin >> decnum;
+		decnum += "\n";
 		fout << decnum;
 	}
-	fout.close(); 
+	fout.close();
+	ifstream fin("decnums.txt"); 
+
+	if (!fin.is_open()) 
+		cout << "Файл не может быть открыт!\n"; 
+	while (fin) {
+		fin >> readnum; 
+		cout << readnum << endl; 
+	}
+		
+	
 	system("pause");
 	task5();
 }
